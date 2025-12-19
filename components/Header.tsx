5eadda90457ffe3f6c726433357e38e7dc68cf33
SongKhuee <148840532+SongKhuee@users.noreply.github.com>
2025-12-19 17:06:01 +0700

    update header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,16 @@
 
-
+import { BellIcon } from "@/assests/Icons"
+import { ArrowDownIcon } from "@/assests/Icons"
+import { UserProfileIcon } from "@/assests/Icons"
 export function Header() {
 
     return (
-        <div className="bg-gray-white flex-col justify-content-center items-center px-6.25">
+        <div className="bg-gray-white flex-col justify-content-center items-center px-6.25 font-display">
             <div className="flex px-3.75 h-24 items-center justify-between">
-                <div className="font-display font-semibold text-4xl text-pink">co
+                <div className="font-semibold text-4xl text-pink">co
                     <span className="text-purple">Mash</span>
                 </div>
-                <div className="flex font-display font-semibold  text-sm text-tgray5 justify-between items-center">
+                <div className="flex font-medium text-lg text-tgray5 justify-between items-center">
                     <div className="px-3 py-2">Nhà chính</div>
                     <div className="px-3 py-2">Sản phẩm</div>
                     <div className="px-3 py-2">Nhập hàng</div>
@@ -19,7 +21,12 @@ export function Header() {
 
                 </div>
                 <div className="flex justify-between items-center">
-                    <div className="bg-pink text-white flex justify-between items-center p-2.5 rounded-lg">Đoàn Lê Vy</div>
+                    <div className="p-3"><BellIcon width={24} height={24}/></div>
+                    <div className="bg-pink text-white flex justify-between items-center p-2.5 rounded-lg">
+                        <div className="p-3"><UserProfileIcon width={30} height={30}/></div>
+                        <div className="text-base font-semibold">Đoàn Lê Vy</div>
+                        <div className="p-3"><ArrowDownIcon width={20} height={20}/></div>
+                    </div>
                 </div>
             </div>
             
