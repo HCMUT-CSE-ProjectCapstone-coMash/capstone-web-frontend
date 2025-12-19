6e3b771b6f93795110aecd6b697cc865d6f90f09
SongKhuee <148840532+SongKhuee@users.noreply.github.com>
2025-12-19 17:17:47 +0700

    Update Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ import { UserProfileIcon } from "@/assests/Icons"
 export function Header() {
 
     return (
-        <div className="bg-gray-white flex-col justify-content-center items-center px-6.25 font-display">
+        <div className="bg-gray-white flex-col justify-center items-center px-6.25 font-display">
             <div className="flex px-3.75 h-24 items-center justify-between">
                 <div className="font-semibold text-4xl text-pink">co
                     <span className="text-purple">Mash</span>
@@ -13,6 +13,7 @@ export function Header() {
                 <div className="flex font-medium text-lg text-tgray5 justify-between items-center">
                     <div className="px-3 py-2">Nhà chính</div>
                     <div className="px-3 py-2">Sản phẩm</div>
+                    <div className="px-3 py-2">Nhân viên</div>
                     <div className="px-3 py-2">Nhập hàng</div>
                     <div className="px-3 py-2">Bán hàng</div>
                     <div className="px-3 py-2">Khuyến mãi</div>
@@ -21,8 +22,8 @@ export function Header() {
 
                 </div>
                 <div className="flex justify-between items-center">
-                    <div className="p-3"><BellIcon width={24} height={24}/></div>
-                    <div className="bg-pink text-white flex justify-between items-center p-2.5 rounded-lg">
+                    <div className="p-3"><BellIcon width={30} height={30}/></div>
+                    <div className="bg-pink text-white flex justify-between items-center rounded-lg">
                         <div className="p-3"><UserProfileIcon width={30} height={30}/></div>
                         <div className="text-base font-semibold">Đoàn Lê Vy</div>
                         <div className="p-3"><ArrowDownIcon width={20} height={20}/></div>
